17fe8c8e4df296886877c73a51c93682663317fe
Bryan Flores <elton.flores.a@uni.pe>
2022-08-19 01:08:51 -0500

    fix: footer overflow

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 export const Footer = () => {
   return (
-    <footer className="font-small py-4 bg-dark text-white ">
-      <div
-        /* style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }} */
-        className="footer-copyright text-center"
-      >
+    <footer className="font-small py-2 bg-dark text-white ">
+      <div className="footer-copyright text-center">
         © 2020 Copyright:
         <Link to="/">littlesop.com</Link>
       </div>
