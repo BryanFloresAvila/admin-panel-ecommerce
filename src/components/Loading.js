e3cba26d1f82f694eea8a436aa71bb10e5671d0d
Bryan Flores <elton.flores.a@uni.pe>
2022-08-22 17:46:18 -0500

    refactor loading  component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,13 +3,7 @@ import React from 'react';
 export const Loading = () => {
   return (
     <div className="d-flex justify-content-center">
-      <div
-        className="spinner-border"
-        style={{ width: '7rem', height: '7rem' }}
-        role="status"
-      >
-        <span className="visually-hidden">Loading...</span>
-      </div>
+      <div className="spinner-border" style={{ width: '7rem', height: '7rem' }} role="status"></div>
     </div>
   );
 };
