f6b3a952f00334e5749936e5e4bdf4cd8fa1ad25
Bryan Flores <elton.flores.a@uni.pe>
2022-08-22 17:43:55 -0500

    add config templates for sl2

---
+++ src/utils/templateForm/templates.js
@@ -0,0 +1,13 @@
+export const categoryTemplate = {
+  _id: '',
+  name: '',
+};
+
+export const productTemplate = {
+  _id: '',
+  name: '',
+  description: '',
+  price: '',
+  category: '',
+  image: '',
+};
